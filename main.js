9a525159027c9df9f0917ed71d182132740bd694
Mohamedmonirshabana <mohamed.monir.shabana@gmail.com>
2021-07-09 17:20:05 +0200

    Add Enum to js file

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
-var count = 5;
-count = 'a';
+var a;
+var b;
+var c;
+var d;
+var e = [1, 3, 4,];
+var f = [1, true, 'a', false];
+var ColorRed = 0;
+var ColorGreen = 1;
+var ColorBlue = 2;
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+;
+var backGroundColor = Color.Red;
