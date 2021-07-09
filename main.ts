5cba937b8c52b1c56b4470bbee577654acbeb7d1
Mohamedmonirshabana <mohamed.monir.shabana@gmail.com>
2021-07-09 18:53:47 +0200

    Add Class For File

--- main.ts
+++ main.ts
@@ -1,19 +1,12 @@
-interface Point{
-    x: number,
-    y: number
+class Point{
+    x: number;
+    y: number;
+    draw() {
+        // ...
+    }
+    getDistance(another: Point) {
+        // ...
+    }
 }
 
-let drawPoint = (point: Point) => {
-    //...
-    
-}
 
-// let drawPoint = (point:{x: number, y: number}) => {
-//     //...
-    
-// }
-
-drawPoint({
-    x: 1,
-    y: 2
-});
