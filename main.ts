fe94b123f4cc6a065c9ee0c027c790164758ac0d
Mohamedmonirshabana <mohamed.monir.shabana@gmail.com>
2021-07-09 20:52:14 +0200

    Add a simple for properties

--- main.ts
+++ main.ts
@@ -5,7 +5,18 @@ class Point{
     draw() {
         console.log('X: '+ this.x+ ' ,y:'+ this.y);
     }
+    getX() {
+        return this.x;
+    }
+
+    setX(value) {
+        if (value < 0)
+            throw new Error("value cannot be less than 0");
+        this.x = value;
+    }
 }
 
 let point = new Point(1, 2);
+let x = point.getX();
+point.setX(10);
 point.draw();
