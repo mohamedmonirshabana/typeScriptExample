a16f4fe7d20de9bc36391b1b78c7ca3e6dd2b4a0
Mohamedmonirshabana <mohamed.monir.shabana@gmail.com>
2021-07-09 18:44:33 +0200

    Add interface For Point

--- main.ts
+++ main.ts
@@ -1,7 +1,17 @@
-let drawPoint = (point) => {
+interface Point{
+    x: number,
+    y: number
+}
+
+let drawPoint = (point: Point) => {
     //...
     
 }
+
+// let drawPoint = (point:{x: number, y: number}) => {
+//     //...
+    
+// }
 
 drawPoint({
     x: 1,
