ce09e03821474c0ef9e8281b5bff51a18ca6ea6b
Mohamedmonirshabana <mohamed.monir.shabana@gmail.com>
2021-07-09 17:25:44 +0200

    Add some way for Code in String

--- main.ts
+++ main.ts
@@ -1,14 +1,6 @@
-let a: number;
-let b: boolean;
-let c: string;
-let d: any;
-let e: number[] = [1, 3, 4,];
-let f: any[] = [1, true, 'a', false];
-
-const ColorRed = 0;
-const ColorGreen = 1;
-const ColorBlue = 2;
-
-enum Color {Red,Green,Blue};
-
-let backGroundColor = Color.Red;
+let message;
+message = 'abc';
+let endwith = <string>message.endsWith('c');
+let alt1 = (<string>message).endsWith('c');
+let alt2 = (message as string).endsWith('c');
+// Other Shape
