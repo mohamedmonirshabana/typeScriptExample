64692fc24fe596e44f11c1fc369dd4fe502164a6
Mohamedmonirshabana <mohamed.monir.shabana@gmail.com>
2021-07-10 19:11:55 +0200

    Compile file and add Target --target ES5

--- like.component.js
+++ like.component.js
@@ -1,11 +1,25 @@
 "use strict";
-exports.__esModule = true;
+Object.defineProperty(exports, "__esModule", { value: true });
 exports.LikeComponent = void 0;
 var LikeComponent = /** @class */ (function () {
-    function LikeComponent(likesCount, isSelected) {
-        this.likesCount = likesCount;
-        this.isSelected = isSelected;
+    function LikeComponent(_likesCount, _isSelected) {
+        this._likesCount = _likesCount;
+        this._isSelected = _isSelected;
     }
+    Object.defineProperty(LikeComponent.prototype, "likesCount", {
+        get: function () {
+            return this._likesCount;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(LikeComponent.prototype, "isSelected", {
+        get: function () {
+            return this._isSelected;
+        },
+        enumerable: false,
+        configurable: true
+    });
     LikeComponent.prototype.onClick = function () {
         // if (this.isSelected) {
         //     this.likesCount--;
@@ -14,8 +28,8 @@ var LikeComponent = /** @class */ (function () {
         //     this.likesCount++;
         //     // this.isSelected = true;
         // }
-        this.likesCount += (this.isSelected) ? -1 : 1;
-        this.isSelected = !this.isSelected;
+        this._likesCount += (this._isSelected) ? -1 : 1;
+        this._isSelected = !this._isSelected;
     };
     return LikeComponent;
 }());
--- point.js
+++ point.js
@@ -1,5 +1,5 @@
 "use strict";
-exports.__esModule = true;
+Object.defineProperty(exports, "__esModule", { value: true });
 exports.Point = void 0;
 var Point = /** @class */ (function () {
     function Point(x, y) {
