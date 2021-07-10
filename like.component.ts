4cb7bd347094126e346876e6032d5c39075de724
Mohamedmonirshabana <mohamed.monir.shabana@gmail.com>
2021-07-10 19:02:59 +0200

    Add new Module for Likes

---
+++ like.component.ts
@@ -0,0 +1,19 @@
+export class LikeComponent{
+
+    constructor(public likesCount: number, public isSelected: boolean) {
+    }
+
+    onClick() {
+        // if (this.isSelected) {
+        //     this.likesCount--;
+        //     // this.isSelected = false;
+        // } else {
+        //     this.likesCount++;
+        //     // this.isSelected = true;
+        // }
+
+        this.likesCount += (this.isSelected) ? 1 : -1;
+
+        this.isSelected = !this.isSelected;
+    }
+}
--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
-import { Point } from './point';
+import { LikeComponent } from "./like.component";
 
-let point = new Point(1, 2);
-point.draw();
+let component = new LikeComponent(10, true);
+component.onClick();
+console.log(`likesCount: ${component.likesCount}, isSelected: ${component.isSelected}`);
