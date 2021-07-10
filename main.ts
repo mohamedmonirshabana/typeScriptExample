d14c3b383ee59b6cf0c74c91ce4d88e5a4cb26c7
Mohamedmonirshabana <mohamed.monir.shabana@gmail.com>
2021-07-10 17:47:12 +0200

    Add new properties for Y

--- main.ts
+++ main.ts
@@ -14,9 +14,20 @@ class Point{
             throw new Error("value cannot be less than 0");
         this.x = value;
     }
+
+    get Y() {
+        return this.y;
+    }
+
+    set Y(value) {
+        if (value < 0)
+            throw new Error("Value can not be less than 0");
+        this.y = value;
+    }
 }
 
 let point = new Point(1, 2);
 let x = point.X;
 point.X = 10;
+point.Y = 20;
 point.draw();
